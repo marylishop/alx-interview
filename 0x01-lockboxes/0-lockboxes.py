ce6d851110617a4a4d7bf0ceb6c15857342b7f0a
“marylishop” <marylishop@yahoo.com>
2024-02-12 23:47:04 +0400

    add tasks to test

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    You have n number of locked boxes in front of you. Each box is numbered
+sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """ Method that determines if all boxes can be opened """
+
+    for key in range(1, len(boxes)):
+        flag = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                flag = True
+                break
+        if not flag:
+            return False
+
+    return True
