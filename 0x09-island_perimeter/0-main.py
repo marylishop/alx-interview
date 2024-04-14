f0a6f450eabda7a384de114cedaf6030d345fcb7
“marylishop” <marylishop@yahoo.com>
2024-04-14 15:29:56 +0400

    making changes

---
+++ 0x09-island_perimeter/0-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+0-main
+"""
+island_perimeter = __import__('0-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid1 = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    grid2 = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+    grid3 = [[1]]
+    grid4 = [[1, 0]]
+    print(island_perimeter(grid1))
+    print(island_perimeter(grid2))
+    print(island_perimeter(grid3))
+    print(island_perimeter(grid4))
